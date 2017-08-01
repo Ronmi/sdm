10d4c2f1c457609d51eda0f7a535bead265235b6
Ronmi Ren <ronmi.ren@gmail.com>
2017-08-02 01:20:37 +0800

    correct way to implement universal wrapper

--- driver/wrappers.go
+++ driver/wrappers.go
@@ -7,25 +7,39 @@ import (
 
 type wrapper reflect.Value
 
-func (w wrapper) Scan(src interface{}) error {
-	vs := reflect.ValueOf(src)
-	v := reflect.Value(w)
-	if src == nil {
+func scanPointer(vs, v reflect.Value, src interface{}) error {
+	if vs.Type().Kind() == reflect.Ptr && vs.IsNil() {
 		v.Set(reflect.Zero(v.Type()))
 		return nil
 	}
 
-	if !v.Elem().IsValid() {
-		// allocate if not initial alocated
-		v.Set(reflect.New(v.Type().Elem()))
-	}
-	v.Elem().Set(vs)
+	return scanValue(vs, v.Elem(), src)
+}
+
+func scanValue(vs, v reflect.Value, src interface{}) error {
+	v.Set(vs)
 	return nil
+}
+
+func (w wrapper) Scan(src interface{}) error {
+	vs := reflect.ValueOf(src)
+	v := reflect.Value(w)
+
+	// allocate space if needed
+	if !v.IsValid() {
+		v.Set(reflect.New(v.Type()))
+	}
+
+	if v.Type().Kind() == reflect.Ptr {
+		return scanPointer(vs, v, src)
+	}
+
+	return scanValue(vs, v, src)
 }
 
 func (w wrapper) Value() (ret sqlDriver.Value, err error) {
 	v := reflect.Value(w)
-	if v.IsNil() || !v.Elem().IsValid() {
+	if v.Type().Kind() == reflect.Ptr && (v.IsNil() || !v.Elem().IsValid()) {
 		return nil, nil
 	}
 
