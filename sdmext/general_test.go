d73ddcd5506cad852aec5cdd2f9eae372c7690f1
Ronmi Ren <ronmi.ren@gmail.com>
2017-11-07 20:14:50 +0800

    add more test

--- sdmext/general_test.go
+++ sdmext/general_test.go
@@ -62,3 +62,42 @@ func egok(m *sdm.Manager, t *testing.T) {
 		t.Errorf("expected F to be 'string3', get %s", string(actual.F))
 	}
 }
+
+func egwithempty(m *sdm.Manager, t *testing.T) {
+	f := func(k string) string {
+		return ""
+	}
+
+	ext := new(ExtGeneral)
+	actual := testok{
+		A: 1,
+		B: 1.1,
+		C: true,
+		D: "string",
+		E: []byte("string2"),
+		F: []rune("string3"),
+	}
+	m.Ext(actual, ext)
+	if err := ext.ReadTo(&actual, f); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if actual.A != 1 {
+		t.Errorf("expected A to be 1, get %d", actual.A)
+	}
+	if actual.B != 1.1 {
+		t.Errorf("expected B to be 1.1, get %f", actual.B)
+	}
+	if !actual.C {
+		t.Errorf("expected C to be true, get %v", actual.C)
+	}
+	if actual.D != "string" {
+		t.Errorf("expected D to be 'string', get %s", actual.D)
+	}
+	if string(actual.E) != "string2" {
+		t.Errorf("expected E to be 'string2', get %s", actual.E)
+	}
+	if string(actual.F) != "string3" {
+		t.Errorf("expected F to be 'string3', get %s", string(actual.F))
+	}
+}
