c3aff8aa63f5354b82d2a98a4244617c41b87275
Ronmi Ren <ronmi.ren@gmail.com>
2018-04-19 20:54:39 +0800

    add an example for ExtGeneral

--- sdmext/general_test.go
+++ sdmext/general_test.go
@@ -1,6 +1,8 @@
 package sdmext
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/Ronmi/sdm"
@@ -100,4 +102,35 @@ func egwithempty(m *sdm.Manager, t *testing.T) {
 	if string(actual.F) != "string3" {
 		t.Errorf("expected F to be 'string3', get %s", string(actual.F))
 	}
+}
+
+func ExampleExtGeneral() {
+	// a struct for test
+	type myStruct struct {
+		A int    `sdm:"a"`
+		B string `sdm:"b"`
+	}
+
+	// create fake request for demo purpose
+	r, _ := http.NewRequest("GET", "https://example.com/?a=1&b=test", nil)
+
+	// create sdm.Manager and register our struct
+	driver.MySQLStub()
+	m := sdm.New(nil, "mysqlstub")
+	m.Reg(myStruct{})
+
+	// here's code should look like in your http handler
+	var formData myStruct
+	ext := &ExtGeneral{}
+	m.Ext(formData, ext)
+
+	r.ParseForm()
+	if err := ext.ReadTo(&formData, r.FormValue); err != nil {
+		// handle error here
+		fmt.Print(err)
+	}
+
+	// dump struct for testing purpose
+	fmt.Printf("%+v", formData)
+	// output: {A:1 B:test}
 }
