d95c752fa6ff5d3146d2f4199853712012a3b5ef
Ronmi Ren <ronmi.ren@gmail.com>
2018-11-06 00:33:14 +0800

    add a helper interface

---
+++ interface.go
@@ -0,0 +1,22 @@
+package sdm
+
+import (
+	"database/sql"
+
+	"github.com/Ronmi/sdm/driver"
+)
+
+// Executable abstracts common facilities between manager and Tx.
+//
+// If you are writing some queries which supports both transaction and
+// non-transaction mode should depend on this instead of Manager/Tx.
+type Executable interface {
+	Query(typ interface{}, qstr string, args ...interface{}) *Rows
+	QueryRow(data interface{}, qstr string, args ...interface{}) error
+	Prepare(data interface{}, qstr string) (*Stmt, error)
+	PrepareSQL(data interface{}, tmpl string, qType driver.QuotingType) (*Stmt, error)
+	Insert(data interface{}) (sql.Result, error)
+	Update(data interface{}, where string, whereargs ...interface{}) (sql.Result, error)
+	Delete(data interface{}) (sql.Result, error)
+	RunBulk(b Bulk) (sql.Result, error)
+}
